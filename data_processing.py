a7a18c853597998df248da8d3b12fc27c5af689c
Bernardo Trevizan <trevizanbernardo@gmail.com>
2019-11-14 15:25:37 -0300

    Normalize datasets, except brca, and input data according to class mean

--- data_processing.py
+++ data_processing.py
@@ -1,18 +1,31 @@
 from pandas import read_csv, DataFrame, concat
 import numpy as np
 
+
+def normalize(data):
+    for j in range(data.shape[1]):
+        dmin = np.min(data[:, j])
+        data[:, j] = (data[:, j] - dmin) / (np.max(data[:, j]) - dmin)
+
+    return data
+
 # Remove id column and replace missing values for the mean value of the attribute
-cancer = read_csv('tests/database/breast-cancer-wisconsin.data', index_col=0, header=None, na_values='?')
+cancer = read_csv('tests/datasets/breast-cancer-wisconsin.data', index_col=0, header=None, na_values='?')
+
 rows, cols = np.where(cancer.isnull() == True)
+
 cancer = cancer.values
+classes = np.unique(cancer[:, -1])
 
 for j in set(cols):
-    cancer[rows, j] = np.nanmean(cancer[:, j])
+    for c in classes:
+        r = np.where(cancer[:, -1] == c)
+        cancer[rows, j] = np.nanmean(cancer[r, j])
 
-DataFrame(cancer).to_csv('tests/database/breast-cancer-wisconsin-processed.csv', header=False, index=False)
+DataFrame(cancer).to_csv('tests/datasets/breast-cancer-wisconsin-processed.csv', header=False, index=False)
 
 # Encode labels and remove zeroed feature
-iono = read_csv('tests/database/ionosphere.data', index_col=None, header=None).values
+iono = read_csv('tests/datasets/ionosphere.data', index_col=None, header=None).values
 iono = np.delete(iono, 1, 1)
 classes = np.unique(iono[:, -1])
 
@@ -20,14 +33,23 @@ for k, c in enumerate(classes):
     i = np.where(iono[:, -1] == c)
     iono[i, -1] = k
 
-DataFrame(iono).to_csv('tests/database/ionosphere-processed.csv', header=False, index=False)
+iono = normalize(iono)
+DataFrame(iono).to_csv('tests/datasets/ionosphere-processed.csv', header=False, index=False)
 
 # Convert to csv and remove header
-pima = read_csv('tests/database/pima.tsv', index_col=None, header=0, sep='\t')
-pima.to_csv('tests/database/pima-processed.csv', header=False, index=False, sep=',')
+pima = read_csv('tests/datasets/pima.tsv', index_col=None, header=0, sep='\t').values
+
+for j in range(pima.shape[1]):
+    dmax = np.max(pima[:, j])
+    pima[:, j] = (pima[:, j] - dmax) / (dmax - np.min(pima[:, j]))
+
+pima = normalize(pima)
+DataFrame(pima).to_csv('tests/datasets/pima-processed.csv', header=False, index=False, sep=',')
 
 # Put target column as last column
-wine = read_csv('tests/database/wine.data', index_col=None, header=None)
+wine = read_csv('tests/datasets/wine.data', index_col=None, header=None)
 x, y = wine.iloc[:, 1:], wine.iloc[:, 0]
+wine = concat([x, y], axis=1).values
 
-concat([x, y], axis=1).to_csv('tests/database/wine-processed.csv', header=False, index=False)
+wine = normalize(wine)
+DataFrame(wine).to_csv('tests/datasets/wine-processed.csv', header=False, index=False)
